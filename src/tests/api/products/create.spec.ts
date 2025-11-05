56ae38fba8f3413dea515283ff126d1c249ba6a3
Tatyana Moskvina <moskvina.ta77@gmail.com>
2025-11-05 20:55:47 +0400

    Completed task-1 and task-2

--- src/tests/api/products/create.spec.ts
+++ src/tests/api/products/create.spec.ts
@@ -5,6 +5,7 @@ import { STATUS_CODES } from "data/statusCodes.js";
 import _ from "lodash";
 import { validateResponse } from "utils/validation/validateResponse.utils.js";
 import { IProduct } from "data/types/product.types.js";
+import { faker } from "@faker-js/faker";
 
 test.describe("[API] [Sales Portal] [Products]", () => {
   let id = "";
@@ -14,7 +15,7 @@ test.describe("[API] [Sales Portal] [Products]", () => {
     if (id) await productsApiService.delete(token, id);
   });
 
-  test("Create Product", async ({ loginApiService, productsApi }) => {
+  test("Create Product with all fields [POSITIVE]", async ({ loginApiService, productsApi }) => {
     token = await loginApiService.loginAsAdmin();
     const productData = generateProductData();
     const createdProduct = await productsApi.create(productData, token);
@@ -31,14 +32,79 @@ test.describe("[API] [Sales Portal] [Products]", () => {
     expect(_.omit(actualProductData, ["_id", "createdOn"])).toEqual(productData);
   });
 
-  test("NOT create product with invalid data", async ({ loginApiService, productsApi }) => {
+
+  test("Create product without notes [POSITIVE]", async ({ loginApiService, productsApi }) => {
     token = await loginApiService.loginAsAdmin();
     const productData = generateProductData();
-    const createdProduct = await productsApi.create({ ...productData, name: 123 } as unknown as IProduct, token);
+    const productWithoutNotes = _.omit(productData, ["notes"])
+    const createdProduct = await productsApi.create({
+      ...productWithoutNotes as unknown as IProduct
+    }, token);
+    validateResponse(createdProduct, {
+      status: STATUS_CODES.CREATED,
+      schema: createProductSchema,
+      IsSuccess: true,
+      ErrorMessage: null,
+    });
+
+    id = createdProduct.body.Product._id;
+
+    const actualProductData = createdProduct.body.Product;
+    expect(_.omit(actualProductData, ["_id", "createdOn"])).toEqual(productWithoutNotes);
+  });
+
+
+
+  test("NOT to create product with incorrect types of fields [NEGATIVE]", async ({ loginApiService, productsApi }) => {
+    token = await loginApiService.loginAsAdmin();
+    const productData = generateProductData();
+    const createdProduct = await productsApi.create({
+      ...productData,
+      name: 123,
+      manufacturer: 123,
+      price: "123",
+      amount: "234",
+      notes: 345
+    } as unknown as IProduct, token);
     validateResponse(createdProduct, {
       status: STATUS_CODES.BAD_REQUEST,
       IsSuccess: false,
       ErrorMessage: "Incorrect request body",
     });
   });
+
+  test("NOT to create product without required fields [NEGATIVE]", async ({ loginApiService, productsApi }) => {
+    token = await loginApiService.loginAsAdmin();
+    const productData = generateProductData();
+    const emptyProduct = _.omit(productData, ["name", "amount", "price", "manufacturer"])
+    const createdProduct = await productsApi.create({
+      ...emptyProduct
+    } as unknown as IProduct, token);
+    validateResponse(createdProduct, {
+      status: STATUS_CODES.BAD_REQUEST,
+      IsSuccess: false,
+      ErrorMessage: "Incorrect request body",
+    });
+  });
+
+  test("NOT to create product with invalid values [NEGATIVE]", async ({ loginApiService, productsApi }) => {
+    token = await loginApiService.loginAsAdmin();
+    const productData = generateProductData();
+    const invalidProduct = {
+      ...productData,
+      name: "Bo",
+      manufacturer: "KFC",
+      price: 0,
+      amount: 1000,
+      notes: faker.string.alphanumeric({ length: 251 })
+    } as unknown as IProduct
+    const createdProduct = await productsApi.create(
+      invalidProduct, token);
+    validateResponse(createdProduct, {
+      status: STATUS_CODES.BAD_REQUEST,
+      IsSuccess: false,
+      ErrorMessage: "Incorrect request body",
+    });
+  });
+
 });
